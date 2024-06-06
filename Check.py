b71d2a9c56f7ba65141ffaa92b674f4fe354be27
Marchcool <167979386+Marchcool@users.noreply.github.com>
2024-06-06 11:52:29 +0700

    Check.py

--- Check.py
+++ Check.py
@@ -1,8 +1,25 @@
-import sympy
+def check_prime_numbers(filename):
+    with open(filename, 'r') as file:
+        prime_numbers = [int(line.strip()) for line in file]
+    return prime_numbers
 
-with open('primes_0-1000000.txt', 'r') as file:
-    numbers = [int(line.strip()) for line in file]
+def is_prime(n):
+    if n <= 1:
+        return False
+    elif n <= 3:
+        return True
+    elif n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
 
-all_primes = all(sympy.isprime(number) for number in numbers)
-
-print("All numbers are prime:", all_primes)
+if __name__ == "__main__":
+    filename = 'prime_numbers.txt'
+    prime_numbers = check_prime_numbers(filename)
+    for prime in prime_numbers:
+        if not is_prime(prime):
+            print(f"{prime} is not a prime number!")
